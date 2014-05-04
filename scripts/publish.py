68f84575db9406fc28979f87ae56128c7dde8972
Sasha Mazurov <alexander.mazurov@gmail.com>
2014-05-04 20:12:09 +0200

    Prepare v5.0. Enlarged result plots.

---
+++ scripts/publish.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+ver = open("VERSION","r").readline()
+is_win = os.name == 'nt'
+
+name="chib_note_v%s.pdf" % ver
+source="./build/chib_note.pdf"
+
+if is_win:
+    target_www = "H:/www/%s" %name
+else:
+    target_www = "/afs/cern.ch/user/a/amazurov/www/%s" % name
+
+target_local="./build/%s" % name
+
+shutil.copyfile(source, target_local)
+print("File '%s' copied to '%s'" % (source, target_local))
+shutil.copyfile(source, target_www)
+print("File '%s' copied to '%s'" % (source, target_www))
+print("Url: http://cern.ch/amazurov/%s" % name)
